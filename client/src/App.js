dd028a240bae2a1ca09d4b2f246304608feca06f
Andrew <8879789+manleyac@users.noreply.github.com>
2019-02-06 19:04:29 -0500

    updating API

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Nav from "./components/Nav";
+import Books from "./pages/Books";
 
 class App extends Component {
   render() {
     return (
-      
+      <div>
+        <Nav />
+        <Books />
+      </div>
     );
   }
 }
---
+++ client/src/pages/Books/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import API from ""
+
+class Book extends Component {
+   state = {
+
+   };
+}
